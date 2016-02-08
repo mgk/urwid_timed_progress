e3f50ae59007b521a5192922ff654d70dd22da4d
Michael Keirnan <michael@keirnan.com>
2016-02-08 12:01:40 -0500

    support multiple units with auto selection of best one
    units param to constructor now accepts a list of units and labels in addition
    to just a string. Passing just a string works as before. API doc has details.
    
    Add example showing multiple units.

---
+++ examples/reasonable_units.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from time import sleep
+import urwid as uw
+from urwid_timed_progress import TimedProgressBar
+
+# Demo of bar adjusting to reasonable units
+# The demo simulates progress at different rates to show
+# the progress bar changing units as needed.
+if __name__ == '__main__':
+
+    palette = [
+        ('normal',   'white', 'black', 'standout'),
+        ('complete', 'white', 'dark magenta'),
+    ]
+
+    units = [
+        ('B', 1),
+        ('KB', 1000),
+        ('MB', 1000000)
+    ]
+    bar = TimedProgressBar('normal', 'complete', done=5e7, units=units)
+    status = uw.Text('simulation that demonstrates auto selecting best units')
+    footer = uw.Text('q to exit, r to run simulation')
+    progress = uw.Frame(uw.ListBox([bar, uw.Divider(bottom=5), status]),
+                        footer=footer)
+
+    def run():
+        bar.reset()
+
+        # start with slow rate of progress
+        status.set_text('starting out, running slowly')
+        for i in range(5):
+            bar.add_progress(1)
+            loop.draw_screen()
+            sleep(.5)
+        for i in range(50):
+            bar.add_progress(200)
+            loop.draw_screen()
+            sleep(.1)
+
+        # run fast until done
+        status.set_text('running fast until done')
+        while bar.current < bar.done:
+            bar.add_progress(100000)
+            loop.draw_screen()
+            sleep(.01)
+
+        status.set_text('done')
+        loop.draw_screen()
+
+    def keypress(key):
+        if key == 'q':
+            raise uw.ExitMainLoop()
+        elif key == 'r':
+            run()
+
+    loop = uw.MainLoop(progress, palette, unhandled_input=keypress)
+    loop.run()
--- urwid_timed_progress/__init__.py
+++ urwid_timed_progress/__init__.py
@@ -3,6 +3,8 @@
 import urwid as uw
 import time
 import datetime
+from operator import itemgetter
+from decimal import Decimal
 
 __version__ = '1.0.2'
 
@@ -16,10 +18,13 @@ class FancyProgressBar(uw.ProgressBar):
 
     def get_text(self):
         """Return extended progress bar text"""
-        return '{} of {} {} ({}%)'.format(self.current,
-                                          self.done,
-                                          self.units,
-                                          int(self.current * 100 / self.done))
+        done_units = to_reasonable_unit(self.done, self.units)
+        current = round(self.current / done_units['multiplier'], 2)
+        percent = int(self.current * 100 / self.done)
+        return '{0:.2f} of {1:.2f} {2} ({3}%)'.format(current,
+                                                      done_units['val'],
+                                                      done_units['label'],
+                                                      percent)
 
 
 class TimedProgressBar(uw.Columns):
@@ -39,8 +44,24 @@ class TimedProgressBar(uw.Columns):
         foreground of satt corresponds to the normal part and the
         background corresponds to the complete part.  If satt  is ``None``
         then no smoothing will be done.
-    :param units: units label
-    :type units: str
+    :param units: units to use. If a string, the units value is taken as a
+        label and is used as is. `units` can also be a list of tuples such
+        as::
+
+            [
+                ('KB', 1000),
+                ('MB', 1000000),
+                ('GB', 1000000000)
+            ]
+
+        With a list of tuples, the timed progress bar selects the most
+        reasonable unit to use for current, done, and rate. 3 KB is more
+        reasonable than .003 MB and 3MB is more reasonable than 3000KB.
+
+        Values greater than 1 and less than 1000 are preferred over fractions.
+        So 400 KB is considered more reasonable than .4 MB.
+
+    :type units: str or list of tuples
     :param label: label shown to the left of the progress bar
     :param label_width: fixed width of `label`. Can be used to align
         stacked progress bars. Set `label_width` to 0 and `label` to the
@@ -49,6 +70,9 @@ class TimedProgressBar(uw.Columns):
     def __init__(self, normal, complete, done=100, satt=None,
                  units='', label='Progress', label_width=15):
         super(TimedProgressBar, self).__init__([], dividechars=1)
+        if isinstance(units, str):
+            units = [(units, 1)]
+        self.units = units
         self.bar = FancyProgressBar(normal, complete, done, satt, units)
 
         self.rate_display = uw.Text('', align='left')
@@ -114,7 +138,8 @@ class TimedProgressBar(uw.Columns):
 
     @property
     def rate_text(self):
-        return '{} {}/s'.format(round(self.rate), self.bar.units)
+        rate_units = to_reasonable_unit(self.rate, self.units)
+        return '{0:.2f} {1}/s'.format(rate_units['val'], rate_units['label'])
 
     @property
     def remaining_time(self):
@@ -137,3 +162,21 @@ class TimedProgressBar(uw.Columns):
             return str(datetime.timedelta(seconds=round(self.remaining_time)))
         except:
             return ''
+
+
+def to_reasonable_unit(value, units, round_digits=2):
+    """Convert a value to the most reasonable unit.
+
+    The most reasonable unit is roughly the one with the smallest exponent
+    absolute value when written in scientific notation. For example
+    `1.5` is more reasonable that `.0015` and `22` is more reasonable
+    than `22000`. There is a bias towards numbers > 1, so `3.2` is
+    considered more reasonable that `.32`.
+    """
+    def to_unit(unit):
+        return float(value) / unit[1]
+    exponents = [abs(Decimal(to_unit(u)).adjusted() - 1) for u in units]
+    best = min(enumerate(exponents), key=itemgetter(1))[0]
+    return dict(val=round(to_unit(units[best]), round_digits),
+                label=units[best][0],
+                multiplier=units[best][1])
